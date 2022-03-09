a5c77f108515514199a6185479a11a0f3a4b3882
Achrou <2643053021@qq.com>
2022-03-09 14:03:13 +0800

    调整root page id

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '3ff5e4c1eb9541428a00ae5ffe8698b4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
